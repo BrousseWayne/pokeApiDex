5198150bbade6fba0e0ffdb140403d667cee26df
BrousseWayne <samynedir@gmail.com>
2025-04-22 11:22:31 +0200

    boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import "./index.css";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "./components/ui/sidebar";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { Button } from "./components/ui/button";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+function App() {
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+      </SidebarProvider>
+    </>
+  );
+}
+
+export default App;

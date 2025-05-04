494bffb49fd6c2ff33f3a5ba802f082ffb4cc869
BrousseWayne <samynedir@gmail.com>
2025-05-05 00:03:30 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import "./index.css";
 import { Input } from "./components/ui/input";
-import { useState } from "react";
+import { forwardRef, SVGProps, useEffect, useState } from "react";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { TypeButton } from "./components/ui/typeButton";
 import { Button } from "./components/ui/button";
+import { Disc2 } from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarProvider,
   SidebarTrigger,
 } from "./components/ui/sidebar";
@@ -30,6 +34,27 @@ import {
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Link, Outlet, useLocation } from "react-router";
+
+interface CustomIconProps extends SVGProps<SVGSVGElement> {
+  size?: number | string;
+}
+
+export const CustomIcon = forwardRef<SVGSVGElement, CustomIconProps>(
+  ({ size = 24, className = "", ...props }, ref) => (
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      width={size}
+      height={size}
+      className={`inline-block fill-current ${className}`}
+      {...props}
+    >
+      <path d="M450.46,256.09C449.35,175.17,399.81,102.71,324,73.79,247.59,44.67,157.49,69,105.82,132.13,54.4,195,46.61,285.58,88.49,355.68c41.8,69.95,123.74,106,203.55,91.63,91-16.37,156.14-98.12,158.35-189.14A20.16,20.16,0,0,0,450.46,256.09ZM119.05,174.38C152.76,118,220.23,87,285,99.43c69.4,13.29,120.43,70.47,128.83,139H318.41c-8.26-27.36-32-48-62.62-48-29.65,0-55.15,20.65-63.11,48H97.74A158,158,0,0,1,119.05,174.38ZM286.13,256.1c-2,38.75-60.67,39.4-60.67,0S284.17,217.33,286.13,256.1Zm24,149.79C246.85,428.58,175,408.74,132.3,356.82a157.53,157.53,0,0,1-34.57-83H192.6c7.91,27.39,33.7,48,63.19,48,30.67,0,54.36-20.68,62.62-48h95.45C406.61,333,367.54,385.32,310.14,405.89Z" />
+    </svg>
+  )
+);
 
 export function DropdownMenuRadioGroupPoke({ query, onValueChange }) {
   return (
@@ -50,6 +75,20 @@ export function DropdownMenuRadioGroupPoke({ query, onValueChange }) {
 function capitalizeFirstLetter(val: string) {
   return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
+
+const sideBarData = [
+  {
+    name: "Pokemon",
+    icon: <CustomIcon size={32} className="white-blue-500" />,
+    url: "/search",
+    // icon: <Disc2 color="#3e9392" style={{ height: 32 }} />,
+  },
+  {
+    name: "Moves",
+    icon: <Disc2 color="#3e9392" style={{ height: 32 }} />,
+    url: "/moves",
+  },
+];
 
 function StatsArray({ stats }) {
   return (
@@ -125,17 +164,68 @@ function TableDemo({ tableData }) {
 function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Search</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {sideBarData.map((data) => (
+                <SidebarMenuItem key={data.name}>
+                  <SidebarMenuButton asChild>
+                    <Link to={data.url}>
+                      {data.icon}
+                      <span>{data.name}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
     </Sidebar>
   );
 }
 
+// function Breadcrumbs() {
+//   console.log(useLocation());
+//   return (
+//     <Breadcrumb>
+//       <BreadcrumbList>
+//         <BreadcrumbItem>
+//           <BreadcrumbLink href="/">Home</BreadcrumbLink>
+//         </BreadcrumbItem>
+//         <BreadcrumbSeparator />
+//         <BreadcrumbItem>
+//           <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
+//         </BreadcrumbItem>
+//         <BreadcrumbSeparator />
+//         <BreadcrumbItem>
+//           <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+//         </BreadcrumbItem>
+//       </BreadcrumbList>
+//     </Breadcrumb>
+//   );
+// }
+
 function Breadcrumbs() {
+  const location = useLocation();
+  const [pathname, setPathname] = useState(location.pathname);
+
+  // Update state when pathname changes
+  useEffect(() => {
+    if (pathname !== location.pathname) {
+      setPathname(location.pathname); // Manually update the state
+    }
+  }, [location.pathname]);
+
+  const pathParts = pathname.split("/").filter(Boolean);
+
+  const breadcrumbs = pathParts.map((part, index) => ({
+    name: capitalizeFirstLetter(part),
+    path: "/" + pathParts.slice(0, index + 1).join("/"),
+  }));
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
@@ -143,13 +233,13 @@ function Breadcrumbs() {
           <BreadcrumbLink href="/">Home</BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink href="/docs/components">Components</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
+        {breadcrumbs.map((breadcrumb) => (
+          <BreadcrumbItem key={breadcrumb.path}>
+            <BreadcrumbLink href={breadcrumb.path}>
+              {breadcrumb.name}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        ))}
       </BreadcrumbList>
     </Breadcrumb>
   );
@@ -203,9 +293,7 @@ function MyForm({ setTableData }) {
   );
 }
 
-function App() {
-  const [tableData, setTableData] = useState([]);
-
+function Layout() {
   return (
     <SidebarProvider>
       <div className="app-layout">
@@ -215,12 +303,8 @@ function App() {
             <SidebarTrigger />
             <Breadcrumbs />
           </div>
-
           <main className="app-content">
-            <div className="center-box">
-              <MyForm setTableData={setTableData} />
-            </div>
-            <div>{<TableDemo tableData={tableData} />}</div>
+            <Outlet />
           </main>
         </div>
       </div>
@@ -228,7 +312,33 @@ function App() {
   );
 }
 
-export default App;
+export function Search() {
+  const [tableData, setTableData] = useState([]);
+
+  return (
+    <>
+      <div className="center-box">
+        <MyForm setTableData={setTableData} />
+      </div>
+      <div>{<TableDemo tableData={tableData} />}</div>
+    </>
+  );
+}
+
+export function Search() {
+  const [tableData, setTableData] = useState([]);
+
+  return (
+    <>
+      <div className="center-box">
+        <MyForm setTableData={setTableData} />
+      </div>
+      <div>{<TableDemo tableData={tableData} />}</div>
+    </>
+  );
+}
+
+export default Layout;
 
 // TODO: add a default paramater for the search
 // TODO: add a placeholder if no sprite
@@ -238,3 +348,4 @@ export default App;
 // TODO: filter longer names
 // TODO: add a page for the forms clickable
 //TODO: modal if no search by ?
+//TODO: animation while fetching
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout, { Search } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Search />} /> */}
+          <Route path="/search" element={<Search />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
